daca1753eb855b28f3b61609724d13cb1054234a
RenosGPS <gpszat@gmail.com>
2023-03-08 12:49:09 +0000

    modified:   __pycache__/celin.cpython-310.pyc 	modified:   celin.py 	modified:   main.py

--- celin.py
+++ celin.py
@@ -7,32 +7,47 @@ in your project
 
 Date 2022 11 26
 
+Date 2023 03 08
+Fix Variables Name to conform code
 """
 
 def ver():
     """
     DOCSTRING HERE
     """
+    display_version = 3
 
     print('AUTHOR  : sirGPS')
     print('DATETIME: 2022/11/26 1630 UTC+02')
     print('LOCATION: NICOSIA 2022')
+    print('VERSION: f{display_version}')
 
 
-def addline(x = 1):
+def addline(x_line = 1):
     """
     Adds a new line or
     multilines
     """
-    for x in range(x):
+    for x_line in range(x_line):
         print(chr(13)) # newline
-        x = x + 1
-          
-def addspace(x = 1):
+        x_line = x_line + 1
+    # v2 whitespace conform to code
+    # v2 snake_case fix
+def addspace(x_space = 1):
     """
     adds space or
     multispaces
     """
-    for x in range(x):
+    for x_space in range(x_space):
         print(chr(32),sep = '',end = '' )
-        x = x + 1
+        x_space = x_space + 1
+    # v2 whitespace conform to code
+    # v2 snake_case fix
+def addbar(x_bar = 1):
+    """
+    Adds a bar accross terminal
+    v2
+    """
+    for x_bar in range(x_bar):
+        print('=',sep = '',end = '')
+        x_bar = x_bar + 1
--- main.py
+++ main.py
@@ -13,34 +13,55 @@ import subprocess
 import celin
 
 
-def MenuAlfa():
+#bar_len = 10
+
+def menu_alfa():
     """
     DOCSTRING HERE
     MENU ALFA CARD
     """
-    
     celin.addline()
-    print('Alfa Menu')
+    celin.addline()
+    print(' Alfa Menu ')
+    celin.addline()
+
     celin.addline()
     # Draw Alfa menu
-
-    print('Q =MainMenu')
-    print('A =Say Alfa')
-
+    print(' Q = MainMenu')
+    print(' A = Say Alfa')
     # Deal User Input
-
     askuser = ' '
     askuser = input('-> ')
     askuser = askuser.lower()
-
     if askuser == 'q':
         main()
     if askuser == 'a':
-        MenuAlfa()
-    
-        
-
-def mainmenucard():
+        print(' ALFA')
+    if askuser != ' ':
+        menu_alfa()
+def menu_beta():
+    """
+    MENU BETA
+    """
+    celin.addline()
+    print(" BETA MENU")
+    celin.addline()
+    print(' Q = MAIN MENU')
+    print(' A = PLACEHOLDER')
+    print(' B = PLACEHOLDER')
+    celin.addline()
+    askuser = ' '
+    askuser = input('->')
+    askuser = askuser.lower()
+    if askuser == 'q':
+        mainmenu_card()
+    if askuser != ' ':
+        menu_beta()
+    if askuser == 'a':
+        print(' menu BETA ALFA PLACE holder')
+    if askuser == 'b':
+        print(' menu BETA BETA')
+def mainmenu_card():
     """
     DOCSTRING HERE
     """
@@ -48,19 +69,16 @@ def mainmenucard():
     print('MainMenu')
     celin.addline()
     #draw card
-    
-    print('Q = Quit')
-    print('R = Restart\n')
-    
-    print('A = Alfa Card')
-    print('B = Beta Card')
-
+    celin.addline()
+    print(' Q = Quit')
+    print(' R = Restart\n')
+    print(' A = Alfa Card')
+    print(' B = Beta Card')
+    celin.addline()
     #deal with user
-
     askuser = ' '
     askuser = input('-> ')
-    askuser = askuser.lower()
-
+    askuser = askuser.lower() # go lowercase all input
     if askuser == 'q':
         print('Quiting...')
         sys.exit()
@@ -68,22 +86,16 @@ def mainmenucard():
         print('Restarting...')
         subprocess.call([sys.executable, os.path.realpath(__file__)] + sys.argv[1:])
     if askuser == 'a':
-        print('Alfa placeholder')
+        menu_alfa()
     if askuser == 'b':
-        print('Beta placeholder')
+        menu_beta()
     if askuser != ' ':
         main()
-        
-
     celin.addline()
-
     main()
-
 def main():
     """
     DOCSTRING HERE
     """
-
-    mainmenucard()
-
+    mainmenu_card()
 main()
